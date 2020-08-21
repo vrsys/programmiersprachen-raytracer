87ca2cd0684325e00f50c84e3a54c7302d03722d
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-08-21 14:56:05 +0200

    Fixed light and material added scene class

--- framework/light.cpp
+++ framework/light.cpp
@@ -1,9 +1,8 @@
 #include "light.hpp"
 
-Light::Light(std::string name, glm::vec3 pos, Color color, float brightness)
-{
-	name_ = name;
-	pos_ = pos;
-	color_ = color;
-	brightness_ = brightness;
-}
+Light::Light(std::string name, glm::vec3 pos, Color color, float brightness):
+name_ { name },
+pos_{ pos },
+color_{ color },
+brightness_{ brightness }{}
+
--- framework/material.cpp
+++ framework/material.cpp
@@ -1,10 +1,8 @@
 #include "material.hpp"
 
-Material::Material(std::string name, Color ka, Color kd, Color ks, float m)
-{
-	name_ = name;
-	ka_ = ka;
-	kd_ = kd;
-	ks_ = ks;
-	m_ = m;
-}
+Material::Material(std::string name, Color ka, Color kd, Color ks, float m):
+	name_{ name },
+	ka_{ ka },
+	kd_{ kd },
+	ks_{ ks },
+	m_{ m }{}
