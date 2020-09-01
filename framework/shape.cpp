941f3971e971ef96917161b4ff1491ff095f57a0
Hasan Yücel <yclhasan00@gmail.com>
2020-09-01 21:54:24 +0200

    TransformRay function added

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -19,6 +19,20 @@ Shape::~Shape()
 	std::cout << "Shape dtor:" << *this << std::endl;
 }
 
+
 std::ostream& operator << (std::ostream& os, Shape const& s) {
 	return s.print(os);
+}
+
+Ray Shape::transformRay(Ray const& ray, glm::mat4 const& mat)
+{    
+	//homogenous coordinates
+	glm::vec4 origin{ ray.origin , 1}; // 1 cause its a point
+	glm::vec4 direction{ ray.direction , 0 }; // 0 cause its a vec
+
+	origin = mat * origin;
+	direction = mat * direction;
+
+	return Ray{ {origin.x, origin.y, origin.z}, glm::normalize(glm::vec3(direction.x, direction.y, direction.z)) };
+
 }
