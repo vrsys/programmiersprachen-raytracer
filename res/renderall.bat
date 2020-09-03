614dd871e50a54bf906d9b9d9c57b446a066dc87
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-09-03 19:45:31 +0200

    Safty backup of working version

---
+++ res/renderall.bat
@@ -0,0 +1,2 @@
+C:\Users\rapha\OneDrive\Documents\Uni\Programiersprachen\programmiersprachen-raytracer\build\source\Debug\raytracer.exe a0000.sdf a0000.ppm
+C:\Users\rapha\OneDrive\Documents\Uni\Programiersprachen\programmiersprachen-raytracer\build\source\Debug\raytracer.exe a0001.sdf a0001.ppm
---
+++ res/renderall.sh
@@ -0,0 +1,2 @@
+/c/Users/rapha/OneDrive/Documents/Uni/Programiersprachen/programmiersprachen-raytracer/build/source/Debug/raytracer.exe a0000.sdf a0000.ppm
+/c/Users/rapha/OneDrive/Documents/Uni/Programiersprachen/programmiersprachen-raytracer/build/source/Debug/raytracer.exe a0001.sdf a0001.ppm
