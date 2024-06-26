c70952b7d30de2e206740634a9c0ecea46cba3f7
ANPL31321 <avkrupiy@gmail.com>
2024-06-26 10:47:25 +0200

    Aufgabe 5.3 Change tests for new constructors

--- source/tests.cpp
+++ source/tests.cpp
@@ -6,11 +6,11 @@
 #include <glm/vec3.hpp>
 
 TEST_CASE(" sphere and box methods ", "[sphere_box_methods]") {
-	Sphere s{ glm::vec3{0.0f, 0.0f, 0.0f}, 12.34f };
+	Sphere s{ "sphere", {0.0f, 0.0f, 0.0f}, glm::vec3{0.0f, 0.0f, 0.0f}, 12.34f };
 	REQUIRE(1913.55123f == Approx(s.area()));
 	REQUIRE(7871.074 == Approx(s.volume()));
 
-	Box b{ glm::vec3{1.2f, 3.4f, 5.6f}, glm::vec3{7.8f, 9.0f, 10.2f} };
+	Box b{ "box", {0.0f, 0.0f, 0.0f}, glm::vec3{1.2f, 3.4f, 5.6f}, glm::vec3{7.8f, 9.0f, 10.2f} };
 	REQUIRE(186.16f == Approx(b.area()));
 	REQUIRE(170.016f == Approx(b.volume()));
 }
