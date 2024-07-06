01e72b242f22e3955235de71428f6a2a56141a60
AntonRoediger <anton.maximilian.roediger@uni-weimar.de>
2024-07-06 19:51:28 +0200

    added operator<< for material

--- framework/material.hpp
+++ framework/material.hpp
@@ -6,6 +6,7 @@
 
 struct Material
 {
+
 	std::string name_{ "placeholder" };
 	Color ka_{ 0, 0, 0 };
 	Color kd_{ 0, 0, 0 };
@@ -13,4 +14,11 @@ struct Material
 	float exponent_{ 0 };
 };
 
+inline std::ostream& operator<<(std::ostream& os, Material const& material) // inline makes it so that the function can be defined within the header file
+{
+	os << "material name: " << material.name_ << " ka: " << "(" << material.ka_.r << ", " << material.ka_.g << ", " << material.ka_.b << ") "
+		<< "kd: " << "(" << material.kd_.r << ", " << material.kd_.g << ", " << material.kd_.b << ") "
+		<< "ks: " << "(" << material.ks_.r << ", " << material.kd_.g << ", " << material.kd_.b << ")";
+	return os;
+}
 #endif
--- framework/shape.cpp
+++ framework/shape.cpp
@@ -15,9 +15,7 @@ Shape::~Shape()
 
 std::ostream& Shape::print(std::ostream& os) const
 {
-	return os << "name: " << name_ << " material: " << "material name: " << material_->name_ << " ka: " << "(" << material_->ka_.r << ", " << material_->ka_.g << ", " << material_->ka_.b << ") " 
-		<< "kd: " << "(" << material_->kd_.r << ", " << material_->kd_.g << ", " << material_->kd_.b << ") " 
-		<< "ks: " << "(" << material_->ks_.r << ", " << material_->kd_.g << ", " << material_->kd_.b << ")" << '\n';
+	return os << "name: " << name_ << " material: " << *material_ << '\n';
 }
 
 std::ostream& operator<<(std::ostream& os, Shape const& s)
